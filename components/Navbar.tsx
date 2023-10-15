2ffb1458e5e59efab3c7b2a96459a6306c29fab7
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-10-15 12:17:44 +0200

    bugfix and add navlink styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,15 +6,19 @@ const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
         <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+          <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+        </Link>
           <ul className="hidden h-full gap-12 lg:flex">
             {NAV_LINKS.map((link) => (
-              <Link href={link.href} key={link.key}>
+              <Link 
+                href={link.href} 
+                key={link.key}
+                className="regular-16 text-grey-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+                >
                 {link.label}
               </Link>
             ))}
           </ul>
-        </Link>
     </nav>
   )
 }
