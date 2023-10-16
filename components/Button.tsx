e3ad4d0b6f4ef4f9a8143afeb099ac4ec71c1d31
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-10-16 09:39:36 +0200

    add Button type and icon

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import Image from "next/image";
+
+type ButtonProps = {
+    type: 'button' | 'submit';
+    title: string;
+    icon?: string;
+    variant: "btn_dark_green";
+}
 
 const Button = ({type, title, icon, variant}: ButtonProps) => {
   return (
     <button
-    type="button"
+    type={type}
     >
-    Button</button>
+     {icon && 
+      <Image src={icon} alt={title} width={24} height={24} />}
+      <label>{title}</label>
+    </button>
   )
 }
 
