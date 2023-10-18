1306b6cf39379918a5b9bc76a4ebcde63abf72de
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-18 13:48:16 +0200

    add distance data to the Map

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -60,10 +60,15 @@ const Hero = () => {
           <div className="relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
               <div className="flex flex-col ">
                 <div className="flexBetween">
-                  <p className="regular-16 text-gray-20">
-                    Location
-                  </p> 
+                  <p className="regular-16 text-gray-20">Location</p> 
                   <Image src="/close.svg" alt="close" width={24} height={24}/>
+                </div>
+
+                <div className="flexBetween">
+                  <div className="flex flex-col">
+                    <p className="regular-16 block text-gray-20">Distance</p>
+                    <p className="bold-20 text-white">173.28 mi</p>
+                  </div>
                 </div>
               </div>
           </div>
