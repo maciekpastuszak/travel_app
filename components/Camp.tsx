19806a7f7c8daf4d8729f753bd533866d8fde8ad
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-19 11:05:54 +0200

    updateCampSite component with props

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,4 +1,4 @@
-const CampSite = () => {
+const CampSite = ({backgroudImage, title, subtitle, peopleJoined}: CampPromps) => {
   return (
     <div>
       Campsite 1
@@ -10,7 +10,12 @@ const Camp = () => {
   return (
     <section className="border-2 border-green-500 2xl:max-container relative flex flex-col p-10 lg:mb-10 lg:py-20 xl:mb-20">
       <div className='hode-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
-        <CampSite />
+        <CampSite 
+          backgroundImage="bg-bg-img-1"
+          title="Putuk Truno Camp"
+          subtitle="Prigen, Pasarum"
+          peopleJoined="50+ Joined"
+        />
         <CampSite />
       </div>
     </section>
