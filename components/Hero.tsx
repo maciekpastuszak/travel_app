1db06792fb284c42efdf6df54728e034082dfbf0
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-19 10:53:59 +0200

    change wrapper to flex

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
       </div>
 
       <div className="relative flex flex-1 items-start border-2 border-blue-500">
-          <div className="relative z-20 w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+          <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
               <div className="flex flex-col ">
                 <div className="flexBetween">
                   <p className="regular-16 text-gray-20">Location</p> 
