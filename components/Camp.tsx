92a80f5c1e6a6c45117137c6791519f4c4cb0568
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-19 11:03:29 +0200

    add two CampSite components to Camp

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+const CampSite = () => {
+  return (
+    <div>
+      Campsite 1
+    </div>
+  )
+}
 
 const Camp = () => {
   return (
     <section className="border-2 border-green-500 2xl:max-container relative flex flex-col p-10 lg:mb-10 lg:py-20 xl:mb-20">
-
+      <div className='hode-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
+        <CampSite />
+        <CampSite />
+      </div>
     </section>
   )
 }
