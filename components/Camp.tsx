c92067aebef3ca79e4d196fd6342c50e08ad1616
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-20 12:56:42 +0200

    clear border

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -48,7 +48,7 @@ const CampSite = ({backgroundImage, title, subtitle, peopleJoined}: CampProps) =
 
 const Camp = () => {
   return (
-    <section className="border-2 border-green-500 2xl:max-container relative flex flex-col p-10 lg:mb-10 lg:py-20 xl:mb-20">
+    <section className="2xl:max-container relative flex flex-col p-10 lg:mb-10 lg:py-20 xl:mb-20">
       <div className='hode-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]'>
         <CampSite 
           backgroundImage="bg-bg-img-1"
