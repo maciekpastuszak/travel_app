614bba8d737811e9c9462690da7b7cb013cba809
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-21 21:24:05 +0200

    add overflow card over camps in Camp

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -63,6 +63,13 @@ const Camp = () => {
           peopleJoined="50+ Joined"
         />
       </div>
+
+      <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+         
+        </div>
+      </div>
+
     </section>
   )
 }
