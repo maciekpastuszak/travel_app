5667eeedcfdf9c0c3a142a5a8770bab77df627d7
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-22 11:56:07 +0200

    add camp data wrapper

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -21,13 +21,23 @@ const Guide = () => {
       </div>
 
       <div className="flexCenter max-container relative w-full border-2 border-blue-500">
-      <Image 
+        <Image 
           src="/boat.png"
           alt="boat"
           width={1440}
           height={580}
           className="w-full object-cover object-center 2xl:rounded-5xl"
         />
+
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween w-full">
+                
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
