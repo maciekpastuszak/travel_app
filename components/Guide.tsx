2e2e584032020881e8ba91ca804275a31a5d4308
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-22 12:02:26 +0200

    add distance img in Guide

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -30,6 +30,14 @@ const Guide = () => {
         />
 
         <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+        <Image 
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+
           <div className="flexBetween flex-col">
             <div className="flex w-full flex-col">
               <div className="flexBetween w-full">
@@ -40,11 +48,8 @@ const Guide = () => {
             </div>
 
             <div className="flex w-full flex-col">
-              
               <p className="regular-16 text-gray-20">Start track</p>
               <h4 className="bold-20 mt-2 whitespace-nowrap">Wonorejo Pasuruan</h4>
-             
-              <p className="bold-20 mt-2">Aguas Calientes</p>
             </div>
           </div>
         </div>
