bc54a5d02bbe7ff809c857952dafaad2bbcd2e4f
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-22 11:06:26 +0200

    add header and paragraph to Guide

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -14,6 +14,10 @@ const Guide = () => {
         <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
           We are here for you
         </p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2>Guide You to Easy Path</h2>
+          <p>Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field. Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain</p>
+        </div>
       </div>
     </section>
   )
