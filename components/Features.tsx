b5b78e3948fdf5a8639e06fb470235a0b0fc3444
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-23 09:16:31 +0200

    add phone img to Features section

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Features = () => {
   return (
     <section className="border-2 border-red-500 flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      
+      <div className="max-container padding-container relative w-full justify-end">
+        <div>
+          <Image 
+            src="/phone.png"
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"
+          />
+        </div>
+      </div>
     </section>
   )
 }
