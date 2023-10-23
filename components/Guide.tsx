23c4729c594487e38976a6a5a55adfc3b09da191
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-23 08:24:14 +0200

    remove unnecessary borders

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Guide = () => {
   return (
     <section className="flexCenter flex-col">
-      <div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+      <div className="padding-container max-container w-full pb-24">
         <Image 
           src="/camp.svg"
           alt="camp"
@@ -20,7 +20,7 @@ const Guide = () => {
         </div>
       </div>
 
-      <div className="flexCenter max-container relative w-full border-2 border-blue-500">
+      <div className="flexCenter max-container relative w-full">
         <Image 
           src="/boat.png"
           alt="boat"
