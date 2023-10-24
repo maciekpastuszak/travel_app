5590c0bf433e3fa5c1228b1bd66f16fb4b523a74
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-24 09:57:44 +0200

    remove variant from the Feature component

--- components/Features.tsx
+++ components/Features.tsx
@@ -33,7 +33,6 @@ const Features = () => {
               key={feature.title}
               title={feature.title} 
               icon={feature.icon}
-              variant={feature.variant}
               description={feature.description} 
             />
           ))}
@@ -47,11 +46,10 @@ const Features = () => {
 type FeatureItem = {
   title: string;
   icon: string;
-  variant: string;
   description: string;
 }
 
-const FeatureItem = ({title, icon, variant, description}: FeatureItem) => {
+const FeatureItem = ({title, icon, description}: FeatureItem) => {
   return (
     <li className="flex w-full flex-1 flex-col items-start">
       <div className="rounded-full p-4 lg:p-7 bg-green-50">
