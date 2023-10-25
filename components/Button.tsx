79d40151ca0230f2e52b7292cdc1210d71f22503
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-25 08:57:21 +0200

    modify button component - pointer

--- components/Button.tsx
+++ components/Button.tsx
@@ -16,7 +16,7 @@ const Button = ({type, title, icon, variant, full}: ButtonProps) => {
     >
      {icon && 
       <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   )
 }
