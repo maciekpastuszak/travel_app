e4937fcd91926d3ac04fb019951d785647fcfd58
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-25 08:43:31 +0200

    add text styles in GetApp

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -5,8 +5,8 @@ const GetApp = () => {
     <section className="flexCenter w-full flex-col pb-[100px]">
       <div className="get-app">
         <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
-          <h2>Get for free now!</h2>  
-          <p>Available on iOS and Android</p>
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now!</h2>  
+          <p className="regular-16 text-gray-40">Available on iOS and Android</p>
         </div>
       </div>
     </section>
