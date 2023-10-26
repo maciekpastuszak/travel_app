22a15ca2f4895ef62dcbcc353af6fd43aead4901
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-10-26 20:32:12 +0200

    add hilink logo img in Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -7,7 +8,7 @@ const Footer = () => {
        <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <Link href="/" className="mb-10">
-           
+            <Image src="hilink-logo.svg" alt="logo" width={74} height={29}/>
           </Link>
         </div>
       </div>
